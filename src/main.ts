562c86bd13bb3efe0019b1cfe7ea8e9a2d23e919
nxsaloj <niisaloj@gmail.com>
2024-11-06 23:52:43 -0600

    Adding todo feature
    Adding icons to the project
    Adding the todo ts file with the logic
    Adding related interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,39 @@
 import './assets/css/main.css'
+import { addTaskToList, setupAddTaskButton } from './todo'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `  
+  <div class="p-5">
+    <div class="container mx-auto mockup-window bg-base-300 border">
+      <div class="join mx-auto">
+        <input id="taskName" type="text" placeholder="Type the task name" class="input input-bordered w-full max-w-xs join-item" />
+        <button id="addTask" class="btn btn-primary text-white join-item">Add</button>        
+      </div>
+      <div class="divider"></div>
+      <div class="overflow-x-auto">
+        <table id="todoList" class="table table-zebra">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th>Completed</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>            
+          </tbody>
+        </table>
+      </div>
+    </div>  
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const addTaskButton = document.getElementById("addTask") as HTMLButtonElement
+const taskNameInput = document.getElementById("taskName") as HTMLInputElement
+const todoList = document.getElementById("todoList") as HTMLTableElement
+
+setupAddTaskButton(addTaskButton, ()=>{
+  const taskName = taskNameInput.value
+  addTaskToList(taskName, todoList)
+  taskNameInput.value = ""
+})
